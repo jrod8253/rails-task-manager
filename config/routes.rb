99c94f4b1f9028b26635e3fdc4f818521d0815e4
Jared Champayne <j4champa@gmail.com>
2023-05-23 16:12:34 +0200

    working site without style

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Create
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  # READ all
+  get "tasks", to: "tasks#index"
+
+  # READ One
+  get "tasks/:id", to: "tasks#show", as: "task"
+
+  # Update
+  get "tasks/:id/edit", to: "tasks#edit", as: "task_edit"
+  patch "tasks/:id", to: "tasks#update"
+
+  # Delete
+  delete "tasks/:id", to: "tasks#destroy", as: "task_delete"
 end
